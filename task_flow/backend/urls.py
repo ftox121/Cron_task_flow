af664cff3c41ed70c6b3089f88f873c311ce11b7
ftox121 <lstaeee@mail.ru>
2025-03-14 18:11:25 +0300

    added swagger

--- task_flow/backend/urls.py
+++ task_flow/backend/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from backend.views import CreateTaskView, DetailView, ExecutionLogListView
@@ -9,4 +10,6 @@ urlpatterns = [
     path('logs/', ExecutionLogListView.as_view(), name='log-list'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
--- task_flow/task_flow/settings.py
+++ task_flow/task_flow/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'backend',
     'rest_framework',
     'django_celery_beat',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -147,7 +148,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 
